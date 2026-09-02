6372b80193f5110c34ed0d3da9b7995f7f65d440
matigianno <matigianno102@gmail.com>
2026-09-02 02:32:15 -0300

    reconsturccion del codigo
    recontruccion por mi parte del codigo luego de ver el video subido a drive.
    implementacion del GET y POST junto con las variables locales y globales adecuadas.

--- server.py
+++ server.py
@@ -1,29 +1,79 @@
-from wsgiref.simple_server import make_server
 import json
+from wsgiref.simple_server import make_server
 
-tasks ={}
-
+tasks = {}
 next_id = 1
+HEDERS = [("Content-Type", "application/json")]
 
-# seccion encargada de obtener los dats de entrada y trabajar con ellos
+# "def" sirve para definir una funcion, en este caso la funcion es "app"
+# que obtiene "environ" y "start_response" como parametros.
+# environ tiene dentro todo lo que se pide al servidor.
 def app(environ, start_response):
-    method = environ['REQUEST_METHOD']
-    path = environ['PATH_INFO']
-    # seccion encargada de leer cuantos segmentos tiene la url que se encuentra luego del verbo
-    # para luego trabajar con ello
-    match method:
-        case ("GET", 1):
-            body = json.dumps(list(tasks.values())).encode('utf-8')
-            start_response('200 OK', [('Content-Type', 'application/json')])
-            return [body]
+    global next_id
 
-        case ("POST", 1):
-            ...
-        case ("PATCH", 1):
-            ...
-        case ("DELETE",1 ):
-            ...
+    info = environ['PATH_INFO']
+    verb = environ['REQUEST_METHOD']
 
-if __name__ == '__main__':
-    server = make_server('localhost', 9292, app)
+    #el 0 es para evitar errores en caso de que el contenido este basio
+    length = int(environ.get('CONTENT_LENGTH',0))
+    length_content = environ['wsgi.input'].read(length)
+
+    parts = info.strip('/').split('/')
+    size = len(parts)
+
+    # chequeo de lo que trae environ y según eso se ejecuta una rama distinta
+    match verb:
+        case 'GET':
+
+            #si hay un solo elemento en la url entonces no se busca ID
+            if size == 1:
+                body = json.dumps(list(tasks.values())).encode('utf-8')
+                start_response('200 OK', HEDERS)
+                return [body]
+
+            #si hay un dos elemento en la url entonces se busca ID
+            elif size == 2:
+                task = tasks.get(int(parts[1]))
+                if task is None:
+                    start_response('404 Not Found', HEDERS)
+                    return [b"Tarea no encontrada"]
+
+                body = json.dumps(task).encode('utf-8')
+                start_response('200 OK', HEDERS)
+                return [body]
+
+            #si no hay nada entonces se da error
+            else:
+                start_response('404 Not Found', HEDERS)
+                return [b"Ruta no encontrada"]
+
+        case 'POST':
+            if size != 1:
+                start_response('404 Not Found', HEDERS)
+                return [b"Ruta no encontrada"]
+
+            else:
+                tasks[next_id] = json.loads(length_content.decode('utf-8'))
+                body = json.dumps(tasks[next_id]).encode('utf-8')
+                next_id = next_id + 1
+
+                start_response('201 Created', HEDERS)
+                return [body]
+
+        case 'PATCH':
+            start_response('501 Not Implemented', HEDERS)
+            return [b"PATCH no implementado"]
+
+        case 'DELETE':
+            start_response('501 Not Implemented', HEDERS)
+            return [b"DELETE no implementado"]
+
+        case _:
+            start_response('405 Method Not Allowed', HEDERS)
+            return [b"Hola"]
+
+
+with make_server("", 9292, app) as server:
+    print("Listening on http://localhost:9292")
+    # lee todas las peticiones que llegan y llama de nuevo a "app"
     server.serve_forever()
